6b57209526303f2e52bf6e7ca75ecc8f6c1290c5
mayurmadan <90102197+mayurmadan@users.noreply.github.com>
2023-11-26 22:27:57 +0530

    Create dynamic-block.tf

---
+++ my-demo/basic-files/dynamic-block/dynamic-block.tf
@@ -0,0 +1,33 @@
+variable "sg_ports" {
+  type        = list(number)
+  description = "list of ingress ports"
+  default     = [8200, 8201,8300, 9200, 9500]
+}
+
+resource "aws_security_group" "dynamicsg" {
+  name        = "dynamic-sg"
+  description = "Ingress for Vault"
+
+  dynamic "ingress" {
+    for_each = var.sg_ports
+    #here interator is name for port 
+    iterator = port
+    content {
+      from_port   = port.value
+      to_port     = port.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+  dynamic "egress" {
+    for_each = var.sg_ports
+    #here interator is Not define by me, so use default dynamic block local name (egress)
+    content {
+      from_port   = egress.value
+      to_port     = egress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+}
